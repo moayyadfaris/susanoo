a0b75ef558833d5cfea1dc996bd98766683f1888
Moayyad Faris <moayyadzf@gmail.com>
2020-06-16 10:36:54 +0300

    Cache by route

--- config/cache.js
+++ config/cache.js
@@ -1,4 +1,4 @@
 module.exports = [
   '/api/v1/countries',
-  '/api/v1/countries/:id' //sample api cache format with route template
+  '/api/v1/countries/:id' // sample api cache format with route template
 ]
