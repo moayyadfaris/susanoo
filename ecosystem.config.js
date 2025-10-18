f5db97efeb8df3093094b273ce139af0df451d47
Moayyad Faris <moayyadzf@gmail.com>
2025-10-19 00:24:52 +0300

    Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'Susanoo Backend',
-    script: 'npm run migrate && npm start',
+    script: 'npm run dev',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
@@ -31,8 +31,8 @@ module.exports = {
     }
   },
   {
-    name: 'QR Code Service',
-    script: 'npm run qr-code-service',
+    name: 'Queue Dashboard',
+    script: 'npm run queue:dashboard',
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
@@ -45,5 +45,20 @@ module.exports = {
       NODE_ENV: 'production'
     }
   }
+  // {
+  //   name: 'QR Code Service',
+  //   script: 'npm run qr-code-service',
+  //   // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+  //   instances: 1,
+  //   autorestart: true,
+  //   watch: false,
+  //   max_memory_restart: '1G',
+  //   env_development: {
+  //     NODE_ENV: 'development'
+  //   },
+  //   env_production: {
+  //     NODE_ENV: 'production'
+  //   }
+  // }
   ]
 }
